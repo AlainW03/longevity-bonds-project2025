1bf17d0e0a5b37384696df3c9378b24d1c5d2004
MLabuschagen <u21626562@tuks.co.za>
2025-10-14 20:34:10 +0200

    Result average coupon amount vs. reference population, completed with dataset and graph

---
+++ Results Average Bond Coupon value vs Reference population.R
@@ -0,0 +1,115 @@
+# This script is used for testing the parameters in the sims controller sheet.
+# This usually regards to setting assumptions and sensitivity testing.
+
+# Since this uses the original Sims Controller script as a template,
+# I will be removing all comments and features that are already explained
+# in the original sheet.
+
+# This is the model's main parameter controls sheet
+# Set the parameters, and run this script at once
+# by pressing Ctrl+Alt+R
+
+# Pre - loop settings
+tests <- 1 # integer at least 1
+results <- c()
+
+for(i in 1:tests){
+  
+  
+  # Parameters that can be changed: 
+  
+  simulations <- 10 #  integer at least 1
+  inital.members <- 100 #  integer at least 10
+  Bond.Proportion <- 100 # value between 0 to 100
+  improv.factor <- 0 # value between -100 to +100
+  coupon.rate <- 12.5 # value between 0 to 100
+  interest <- 10.63 # value between 0 to 100
+  fixed_increase_rate <- 5.5 # value between 0 and 100
+  rate_for_discounting <- interest # value between 0 and 100
+  EPV.mort.risk.margin <- 43.29 # value between 0 to 100
+  reference.population.age <- 65 # integer between 16 to 120
+  Fund.Monitor.Total.columns <- 70 # integer at least 60
+  
+  Sensitivity_tests_loop_counter <- i
+# Targeted Parameter: Risk margin
+# Target Parameter: Prob of ruin 99%
+
+#EPV.mort.risk.margin <-  26 + i*2 
+
+
+# Do not touch variables:
+{
+Feature <- 0 # Should remain 0
+Original.Fund <- c()
+Fund <- c()
+FUND <- as.data.frame(matrix(0,ncol = Fund.Monitor.Total.columns))
+BOND <- as.data.frame(matrix(0,ncol = Fund.Monitor.Total.columns))
+timing <- system.time(source("Merged scripts with Controls.R"))
+time_elapsed <- as.numeric(timing["elapsed"])
+{
+  FUND <- FUND[-1,]
+  FUND <- FUND[,1: (max(  apply(FUND!= 0,MARGIN = 1,FUN = sum) )) ]
+  colnames(FUND) <- paste0("Y",seq_along(FUND[1,]))
+  rownames(FUND) <- paste0("SIM",c(1:(simulations)))
+} #Cleaning up Fund Value monitor
+
+if(Bond.Prop > 0){
+  BOND <- BOND[-1,]
+  BOND <- BOND[,1: (max(  apply(BOND!= 0,MARGIN = 1,FUN = sum) )) ]
+  colnames(BOND) <- paste0("Y",seq_along(BOND[1,]))
+  rownames(BOND) <- paste0("SIM",c(1:(simulations)))
+}else{remove(BOND)} #Cleaning up Bond Value monitor
+
+Original.Fund.Avg <- mean(Original.Fund)
+results2 <- c()
+results3 <- c()
+}
+
+# Crafting and cleaning result
+{result <- as.data.frame(rbind(round(simulations,digits = 0),
+                              round(inital.members,digits = 0),
+                              round(Prob.of.ruin*100, digits = 3),
+                              round(VAR,digits = 3),
+                              round((100*VAR/Original.Fund.Avg),digits = 3),
+                              Bond.Proportion,
+                              improv.factor,
+                              coupon.rate,
+                              ifelse(Feature==1, "Yes","No"),
+                              round(time_elapsed,digits = 3)))
+
+colnames(result) <- "Results"
+rownames(result) <- c("Simulations",
+                      "Initial Nr of Members",
+                      "Prob of Ruin %",
+                      "VAR @ 95%",
+                      "VAR as % of Original Fund",
+                      "Prop invested in Bond",
+                      "Mort Improvement Factor",
+                      "Coupon Rate %",
+                      "Feature active?",
+                      "Runtime")
+
+
+#View(result)
+#View(FUND)
+} 
+
+
+# Results of testing
+sim.and.result <- c(EPV.mort.risk.margin, round(Prob.of.ruin*100, digits = 3),round((100*VAR/Original.Fund.Avg),digits = 3) )
+results <- cbind(results, sim.and.result)
+}
+
+colnames(results) <- seq_along(results[1,])
+rownames(results) <- c("Risk Margin", "Prob of Ruin", "VAR %")
+
+#View(results)
+
+
+# Here is the average bond dataset
+avg.bond <- as.data.frame(apply(BOND, MARGIN = 2, FUN = mean))
+rownames(avg.bond) <- paste0("Y",seq_along(avg.bond[,1]))
+colnames(avg.bond) <- c(paste(c("Average Coupon amount with ref pop ", reference.population.age), collapse = ""))
+plot(x = seq_along(avg.bond[,1]), y = avg.bond[,1], type = "l", xlab = "Years", ylab = "Average Coupon amount", main = paste(c("Average Coupon value overtime with the reference population being at age ", reference.population.age), collapse = ""))
+
+
