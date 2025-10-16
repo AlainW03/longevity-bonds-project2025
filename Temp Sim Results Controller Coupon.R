ee024996c0e42b6bd3b0051c0fbb5276306eac3b
MLabuschagen <u21626562@tuks.co.za>
2025-10-16 09:06:59 +0200

    Activated progress bar, and tweaked script to be functional again

--- Temp Sim Results Controller Coupon.R
+++ Temp Sim Results Controller Coupon.R
@@ -2,15 +2,15 @@
 # Set the parameters, and run this script at once
 # by pressing Ctrl+Alt+R
 
-simulations <- 10
-Bond.Proportion <- 0 #Controls what prop of fund gets invested into a Longevity Bond
-improv.factor <- 0 # factor that messes with kappa's drift value
+simulations <- 1000
+Bond.Proportion <- 100 #Controls what prop of fund gets invested into a Longevity Bond
+improv.factor <- 50 # factor that messes with kappa's drift value
 # It takes a value between -100 to +100. The higher (lower) the value
 # the greater (smaller) the effect of the trend will be.
 results <- c()
-tests <- 1
+tests <- 10
 for(i in 1:tests) {
-coupon.rate <- 0.125 + i/400
+coupon.rate <- 0.1245 + i/400
 {
   # The Prob of Ruin varies between 50% and 0% between
 # a coupon rate of 0.0428 and 0.047
@@ -27,16 +27,17 @@ coupon.rate <- 0.125 + i/400
 inital.members <- 1000
 Original.Fund <- c()
 reference.population.age <- 65
+Sensitivity_tests_loop_counter <- i
 
 # I think I got confused with the rate of return, and the discount rate
 
-interest <- 22  # value between 0 and 100, sweetspot is around 22
+interest <- 10.63  # value between 0 and 100, sweetspot is around 22
 # select mortality risk is reflected in the interest rate. Since, say you have
 # 1000 members, then your select mortality risk is small. You'll see that your i
 # can be anything and it will barely have an affect with 1000 members (PoR of 50%)
-fixed_increase_rate <- 4 # value between 0 and 100
+fixed_increase_rate <- 5.5 # value between 0 and 100
 rate_for_discounting <- interest # value between 0 and 100
-EPV.mort.risk.margin <- 45# value between 0 and 100, allows for trend in EPV
+EPV.mort.risk.margin <- 43.29 # value between 0 and 100, allows for trend in EPV
 # 45 is the best estimate, with a 50% PoR with 1000 members (which theoretically 
 #restricts select risk)
 Fund <- c()
@@ -54,25 +55,28 @@ Fund <- c()
 
 # Creating Fund value monitor
 {
-  Total.columns <- 70
+  Fund.Monitor.Total.columns <- 70
+  Original.Fund <- c()
+  Fund <- c()
+  FUND <- as.data.frame(matrix(0,ncol = Fund.Monitor.Total.columns))
+  BOND <- as.data.frame(matrix(0,ncol = Fund.Monitor.Total.columns))
+  timing <- system.time(source("Merged scripts with Controls.R"))
+  time_elapsed <- as.numeric(timing["elapsed"])
+  {
+    FUND <- FUND[-1,]
+    FUND <- FUND[,1: (max(  apply(FUND!= 0,MARGIN = 1,FUN = sum) )) ]
+    colnames(FUND) <- paste0("Y",seq_along(FUND[1,]))
+    rownames(FUND) <- paste0("SIM",c(1:(simulations)))
+  } #Cleaning up Fund Value monitor
   
-  FUND <- as.data.frame(matrix(0,ncol = Total.columns))
-  
-  
+  if(Bond.Prop > 0){
+    BOND <- BOND[-1,]
+    BOND <- BOND[,1: (max(  apply(BOND!= 0,MARGIN = 1,FUN = sum) )) ]
+    colnames(BOND) <- paste0("Y",seq_along(BOND[1,]))
+    rownames(BOND) <- paste0("SIM",c(1:(simulations)))
+  }else{remove(BOND)} #Cleaning up Bond Value monitor
   
 }
-
-timing <- system.time(source("Merged scripts with Controls.R"))
-
-time_elapsed <- as.numeric(timing["elapsed"])
-
-{
-  FUND <- FUND[-1,]
-  FUND <- FUND[,1: (max(  apply(FUND!= 0,MARGIN = 1,FUN = sum) )) ]
-  colnames(FUND) <- paste0("Y",seq_along(FUND[1,]))
-  rownames(FUND) <- paste0("SIM",c(1:(simulations)))
-} #Cleaning up Fund Value monitor
-
 Original.Fund.Avg <- mean(Original.Fund)
 result <- as.data.frame(rbind(round(simulations,digits = 0),
                               round(inital.members,digits = 0),
@@ -97,8 +101,8 @@ rownames(result) <- c("Simulations",
                       "Feature active?",
                       "Runtime")
 
-prob.plus.interest <- c(interest, Prob.of.ruin)
-results <- cbind(results, prob.plus.interest)
+prob.plus.coupon <- c(coupon.rate, Prob.of.ruin)
+results <- cbind(results, prob.plus.coupon)
 }
 View(result)
 #View(FUND)
